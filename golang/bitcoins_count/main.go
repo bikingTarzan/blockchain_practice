5e70cb0606659bfc8639764a993408bd576c62d0
tarzanCahn <272867557@qq.com>
2019-08-02 17:08:33 +0800

    计算比特币总量

---
+++ golang/bitcoins_count/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	total := 0.0
+	blockinterval := 21.0		//单位：万	比特币每21个区块减半
+	currentReward := 50.0		//第一个区块50个比特币
+
+	for currentReward > 0 {
+		amount1 := blockinterval * currentReward
+
+		currentReward *= 0.5
+
+		total += amount1
+
+	}
+
+	fmt.Printf("比特币总量为:%v 个\n", total)
+
+}
+
+
